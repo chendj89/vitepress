8e067057d76562d09a23c5b4eca5d4446c7a1149
chendj89 <chendj89@hotmail.com>
2022-09-01 09:33:56 +0800

    543442

--- src/components/vp-tool/index.vue
+++ src/components/vp-tool/index.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import { getCurrentInstance, defineAsyncComponent } from "vue";
 import useDialog from "../../dialog/useDialog";
-import vpTop from "./top.vue";
-useDialog(vpTop);
 </script>
 <template>
   <div>
